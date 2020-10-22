1787e6c17dbb8d8063e9d5682facdda16746a261
krae <kraewind@gmail.com>
2020-10-22 11:09:00 -0400

    commit tree a738a65814a7ca62e9f8a6b1e2eb7a6eedf45f6c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end    
+
+def batch_badge_creator(array)
+    array.collect {|name| badge_maker(name)}
+end    
+
+def assign_rooms(speakers)
+    speakers.collect {|speaker| "Hello, #{speaker}! You'll be assigned to room #{speakers.index(speaker) + 1}!"}
+end    
+
+def printer(array)
+    batch_badge_creator(array).each do |name|
+        puts name
+    end    
+    assign_rooms(array).each do |name|
+        puts name
+    end    
+end    
